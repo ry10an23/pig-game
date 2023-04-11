9c9bec723e2b1334f652649d0e296022e9b652b6
ry10an23 <kawaguchi.r823@gmail.com>
2023-04-10 22:41:46 -0700

    start to make the rolling dice function

--- script.js
+++ script.js
@@ -4,8 +4,18 @@
 const score0E1 = document.querySelector('#score--0');
 const score1E1 = document.getElementById('score--1');
 const diceE1 = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 // Starting Conditions
 score0E1.textContent = 0;
 score1E1.textContent = 0;
 diceE1.classList.add('hidden');
+
+// Rolling Dice Functionality
+btnRoll.addEventListener('click', function () {
+  // 1. Generating a random dice roll
+  // 2. Display dice
+  // 3. Check for rolled 1: If true, switch to next player
+});
