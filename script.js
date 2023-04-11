0d3fe264f866e2a93e473bca82dc796aa9f80e78
ry10an23 <kawaguchi.r823@gmail.com>
2023-04-10 22:58:47 -0700

    added the dice number to the current score

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 // Selecting Elements
 const score0E1 = document.querySelector('#score--0');
 const score1E1 = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -13,14 +16,24 @@ score0E1.textContent = 0;
 score1E1.textContent = 0;
 diceEl.classList.add('hidden');
 
+let currentScore = 0;
+
 // Rolling Dice Functionality
 btnRoll.addEventListener('click', function () {
   // 1. Generating a random dice roll
   const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
 
   // 2. Display dice
   diceEl.classList.remove('hidden');
   diceEl.src = `./imgs/dice-${dice}.png`;
 
-  // 3. Check for rolled 1: If true, switch to next player
+  // 3. Check for rolled 1
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore; // CHANGE LATER
+  } else {
+    // Switch to next player
+  }
 });
