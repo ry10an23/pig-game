206eaf2cd48c0a3da183cd23d54dd295d9566c39
ry10an23 <kawaguchi.r823@gmail.com>
2023-04-10 22:37:31 -0700

    default setting

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-"use strict";
+'use strict';
+
+// Selecting Elements
+const score0E1 = document.querySelector('#score--0');
+const score1E1 = document.getElementById('score--1');
+const diceE1 = document.querySelector('.dice');
+
+// Starting Conditions
+score0E1.textContent = 0;
+score1E1.textContent = 0;
+diceE1.classList.add('hidden');
