8a3afe99efca16a1ec1ee438a3a9e8e15b811175
ry10an23 <kawaguchi.r823@gmail.com>
2023-04-10 22:48:35 -0700

    show the random number with dice

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // Selecting Elements
 const score0E1 = document.querySelector('#score--0');
 const score1E1 = document.getElementById('score--1');
-const diceE1 = document.querySelector('.dice');
+const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
@@ -11,11 +11,16 @@ const btnHold = document.querySelector('.btn--hold');
 // Starting Conditions
 score0E1.textContent = 0;
 score1E1.textContent = 0;
-diceE1.classList.add('hidden');
+diceEl.classList.add('hidden');
 
 // Rolling Dice Functionality
 btnRoll.addEventListener('click', function () {
   // 1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
   // 2. Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `./imgs/dice-${dice}.png`;
+
   // 3. Check for rolled 1: If true, switch to next player
 });
