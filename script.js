c11509ce5ef6e3ec95d66f2d15d28ef9b44f253b
ry10an23 <kawaguchi.r823@gmail.com>
2023-04-10 23:26:42 -0700

    switch the current player's bg-colour

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Selecting Elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0E1 = document.querySelector('#score--0');
 const score1E1 = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -42,5 +44,7 @@ btnRoll.addEventListener('click', function () {
     document.getElementById(`current--${activePlayer}`).textContent = 0;
     currentScore = 0;
     activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
